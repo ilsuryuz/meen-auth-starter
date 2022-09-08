bede462a11fcfa0ce29744d47519cd8ebbc073f3
Ilsur <yuzlikeyevi@gmail.com>
2022-09-08 15:39:51 -0500

    Part 1 Complete

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -7,8 +7,36 @@ const User = require('../models/user');
 // ** New (login page) **
 
 // ** Delete (logout route) **
-
+sessionsRouter.delete('/', (req, res) => {
+    req.session.destroy((error) => {
+        res.redirect('/')
+    })
+})
 // ** Create (login route) **
+sessionsRouter.post('/', (req, res) => {
+    // ** Check for existing user **
+    User.findOne({
+        email: req.body.email,
+    }, (error, foundUser) => {
+        if (!foundUser) {
+            res.send(`Oops! No user with that email address has been registered`);
+        } else {
+            // ** If a user has been found **
+            // ** compare the given password with the hashed password we have stored **
+            const passMatches = bcrypt.compareSync(req.body.password, foundUser.password);
+            // ** If the password matches **
+            if (passMatches) {
+                // ** add the user to our session **
+                req.session.currentUser = foundUser;
 
+                // ** redirect back to our home page **
+                res.redirect('/');
+            } else {
+                // ** if the password don't match **
+                res.send('Oops! Invalid credentials.');
+            }
+        }
+    })
+})
 // ** Export Sessions Router **
 module.exports = sessionsRouter;
